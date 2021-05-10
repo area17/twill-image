01849cf6b4047a297b76d9af36509b04eac1c08b
Pablo Barrios <ingpablobarrios@gmail.com>
2021-05-10 20:31:03 -0300

    Update Image controller to accept any object that respond to Image as method

--- src/ImageController.php
+++ src/ImageController.php
@@ -3,7 +3,6 @@
 namespace Croustille\Image;
 
 use Illuminate\View\View;
-use A17\Twill\Models\Model;
 use A17\Twill\Models\Media;
 use Croustille\Image\Models\TwillImageSource;
 use Croustille\Image\Models\Image;
@@ -14,48 +13,48 @@ class ImageController
      * Helper to return a View of component twill-image for responsive fluid
      * images.
      *
-     * @param A17\Twill\Models\Model $model
+     * @param $object
      * @param string $role
      * @param string $crop
      * @param array $args
      * @param A17\Twill\Models\Media $media
      * @return Illuminate\View\View
      */
-    private function image(Model $model, string $role, string $crop = 'default', $args = [], Media $media = null): Image
+    private function image($object, string $role, string $crop = 'default', $args = [], Media $media = null): Image
     {
-        $source = new TwillImageSource($model, $role, $crop, $media);
+        $source = new TwillImageSource($object, $role, $crop, $media);
         $image = new Image($source, $args);
 
         return $image;
     }
 
-    public function fullWidth(Model $model, string $role, string $crop = 'default', $args = [], Media $media = null): View
+    public function fullWidth($object, string $role, string $crop = 'default', $args = [], Media $media = null): View
     {
         $args['layout'] = 'fullWidth';
-        $image = $this->image($model, $role, $crop, $args, $media);
+        $image = $this->image($object, $role, $crop, $args, $media);
 
         return $image->view();
     }
 
-    public function constrained(Model $model, string $role, string $crop = 'default', $args = [], Media $media = null): View
+    public function constrained($object, string $role, string $crop = 'default', $args = [], Media $media = null): View
     {
         $args['layout'] = 'constrained';
-        $image = $this->image($model, $role, $crop, $args, $media);
+        $image = $this->image($object, $role, $crop, $args, $media);
 
         return $image->view();
     }
 
-    public function fixed(Model $model, string $role, string $crop = 'default', $args = [], Media $media = null): View
+    public function fixed($object, string $role, string $crop = 'default', $args = [], Media $media = null): View
     {
         $args['layout'] = 'fixed';
-        $image = $this->image($model, $role, $crop, $args, $media);
+        $image = $this->image($object, $role, $crop, $args, $media);
 
         return $image->view();
     }
 
-    public function getSourceData(Model $model, string $role, string $crop = 'default', $args = [], Media $media = null): array
+    public function getSourceData($object, string $role, string $crop = 'default', $args = [], Media $media = null): array
     {
-        $source = new TwillImageSource($model, $role, $crop, $media);
+        $source = new TwillImageSource($object, $role, $crop, $media);
         $image = new Image($source, $args);
 
         return $image->getSourceData();
