4d97605226cff45f63eb9ee90b7f1b920dafa6d6
Patrick Vézina <patrick@croustille.io>
2021-08-09 15:36:43 -0400

    Rename TwillImageSource to `Source`

--- src/ImageController.php
+++ src/ImageController.php
@@ -3,22 +3,22 @@
 namespace A17\Twill\Image;
 
 use A17\Twill\Models\Media;
-use A17\Twill\Image\Models\TwillImageSource;
+use A17\Twill\Image\Models\Source;
 use A17\Twill\Image\Models\Image;
 
 class ImageController
 {
     public function source($object, $args = [], Media $media = null): array
     {
-        $source = new TwillImageSource($object, $args, $media);
+        $source = new Source($object, $args, $media);
 
         return $source->toArray();
     }
 
-    public function render($data, $args = [])
+    public function render($imageSource, $args = [])
     {
         $args['layout'] = $args['layout'] ?? 'fullWidth';
-        $image = new Image($data, $args);
+        $image = new Image($imageSource, $args);
 
         return $image->view();
     }
