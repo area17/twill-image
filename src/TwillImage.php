85f9070dbdd62ec75888bb8ea13514db0fb05080
Patrick Vézina <patrick@croustille.io>
2021-09-07 13:23:52 -0400

    Render method accept array or Image instance

--- src/TwillImage.php
+++ src/TwillImage.php
@@ -6,6 +6,7 @@ use A17\Twill\Models\Media;
 use A17\Twill\Models\Model;
 use A17\Twill\Image\Models\Image;
 use A17\Twill\Image\ViewModels\ImageViewModel;
+use Illuminate\Contracts\Support\Arrayable;
 
 class TwillImage
 {
@@ -21,12 +22,16 @@ class TwillImage
     }
 
     /**
-     * @param array $data
+     * @param Image|array $data
      * @param array $overrides
      * @return string
      */
     public function render($data, $overrides = [])
     {
+        if ($data instanceof Arrayable) {
+            $data = $data->toArray();
+        }
+
         $viewModel = new ImageViewModel($data, $overrides);
 
         return view('image::wrapper', $viewModel);
