4c840aa9b4f57bf06a3d9ee2987db98300803629
Adam Říha <adam.riha@icloud.com>
2022-03-13 12:36:34 +0100

    The sizes attribute only if srcset is also present

--- resources/views/image.blade.php
+++ resources/views/image.blade.php
@@ -1,14 +1,13 @@
-@php
-$shouldLoad = $shouldLoad ?? true;
-@endphp
 <img
     decoding="async"
     @isset($loading) loading="{{$loading}}" @endisset
-    @if($shouldLoad) src="{{$src}}" @endif
+    @if($shouldLoad = $shouldLoad ?? true) src="{{$src}}" @endif
     @if(!$shouldLoad) data-src="{{$src}}" @endif
-    @if($shouldLoad && isset($srcSet)) srcSet="{{$srcSet}}" @endif
+    @if($shouldLoad && isset($srcSet))
+        @isset($sizes) sizes="{{$sizes}}" @endisset
+        srcset="{{$srcSet}}"
+    @endif
     @if(!$shouldLoad && isset($srcSet)) data-srcset="{{$srcSet}}" @endif
-    @isset($sizes) sizes="{{$sizes}}" @endisset
     @isset($alt) alt="{{$alt}}" @endisset
     @isset($style) style="{{$style}}" @endisset
     @isset($attributes) {!! $attributes !!} @endisset
