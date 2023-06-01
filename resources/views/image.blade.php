abb44dd5507a3f811cb4ecb19bd7f45f62d6b3f1
Patrick Vézina <patrick@croustille.io>
2023-06-01 15:18:11 -0400

    Do not add sizes attr if no srcset on img tag

--- resources/views/image.blade.php
+++ resources/views/image.blade.php
@@ -8,7 +8,7 @@ $shouldLoad = $shouldLoad ?? true;
     @if(!$shouldLoad) data-src="{{$src}}" @endif
     @if($shouldLoad && isset($srcSet)) srcSet="{{$srcSet}}" @endif
     @if(!$shouldLoad && isset($srcSet)) data-srcset="{{$srcSet}}" @endif
-    @isset($sizes) sizes="{{$sizes}}" @endisset
+    @if(isset($srcSet) && isset($sizes)) sizes="{{$sizes}}" @endif
     @isset($alt) alt="{{$alt}}" @endisset
     @isset($style) style="{{$style}}" @endisset
     @isset($attributes) {!! $attributes !!} @endisset
