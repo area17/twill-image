c3ce6e5f003088679c5ca08244cbb33cd51b0032
Patrick Vézina <patrick@croustille.io>
2023-06-01 15:17:28 -0400

    Use img if only one srcset

--- resources/views/picture.blade.php
+++ resources/views/picture.blade.php
@@ -1,7 +1,7 @@
 @php
 $shouldLoad = $shouldLoad ?? true;
 @endphp
-@isset($sources)
+@if(isset($sources) && count($sources) > 1)
     <picture>
         @isset($sources)
             @foreach($sources as $source)
@@ -26,8 +26,19 @@ $shouldLoad = $shouldLoad ?? true;
             'src' => $fallback,
         ])
     </picture>
+@elseif(count($sources) === 1)
+    @php
+    $source = $sources[0];
+    @endphp
+
+    @include('twill-image::image', [
+        'src' => $fallback,
+        'type' => isset($source['type']) && config('twill-image.webp_support') ? $source['type'] : null,
+        'media' => isset($source['mediaQuery']) ? $source['mediaQuery'] : null,
+        'srcSet' => $source['srcset'],
+    ])
 @else
     @include('twill-image::image', [
         'src' => $fallback,
     ])
-@endisset
+@endif
